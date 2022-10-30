b220728e2de6a1518e616e75fcb7168dba28750b
yanyeef <yanyeef@nycstudents.net>
2022-10-30 11:07:51 -0400

    changes

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Calculator {
+public class Calculator {
+    private double one;
+    private double two;
+
+    public Calculator(double one, double two) {
+        this.one = one;
+        this.two = two;
+    }
+
+    public double multiply() {
+        return one*two;
+    }
+    public double divide() {
+        double div = one/two;
+        return div;
+    }
+    public double add () {
+        return one + two;
+    }
+    public double sub() {
+        return one - two;
+    }
+    public double mod() {
+        return ( one % two);
+    }
+    public double sqrt() {
+        return Math.pow(one, two);
+    }
+
 }
--- src/CalculatorRunner.java
+++ src/CalculatorRunner.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class CalculatorRunner {
+import java.util.Scanner;
+public class CalculatorRunner {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter an expression with *, /, +, -, %, or ^:" );
+        String equ = scan.nextLine();
+
+        String one = equ.substring(0, equ.indexOf(" "));
+        double first = Double.parseDouble(one);
+        String two = equ.substring(equ.indexOf(" ") + 2);
+        double second = Double.parseDouble(two);
+        String sign = equ.substring(equ.indexOf(" ")+ 1, equ.indexOf(" ") + 2);
+
+        Calculator cal = new Calculator(first, second);
+        if (sign.equals("/")) {
+            System.out.println(cal.divide());
+        } else if  (sign.equals("*")) {
+            System.out.println(cal.multiply());
+        }else if (sign.equals("+")) {
+            System.out.println(cal.add());
+        } else if (sign.equals("-")) {
+            System.out.println(cal.sub());
+        }else if (sign.equals("%")) {
+            System.out.println(cal.mod());
+        }else if (sign.equals("^")) {
+            System.out.println(cal.sqrt());
+        }
+
+    }
 }
